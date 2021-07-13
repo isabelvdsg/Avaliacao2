f303008d7810bf5c74f55bcc03461cdde065335e
isabelvdsg <86683266+isabelvdsg@users.noreply.github.com>
2021-07-13 13:20:14 -0300

    Update processaCad.php

--- processaCad.php
+++ processaCad.php
@@ -11,13 +11,13 @@
     } else {
         echo "Conexão realizada.";
         if($_REQUEST){
+            $nomeCompleto = $_POST["name"];
             $nomeDeUsuario = $_POST["user"];
             $email = $_POST["email"];
             $senha = $_POST["password"];
             
-            // Apenas para fins DIDÁTICOS. Procedimento inseguro.
-            // Código propenso a SQL Injection.
-            $sql = "INSERT INTO usuarios (nome_usuario, email, senha) VALUES ('$nomeDeUsuario', '$email', '$senha')";
+    
+            $sql = "INSERT INTO usuarios (nome_completo, nome_usuario, email, senha) VALUES ('$nomeCompleto','$nomeDeUsuario', '$email', '$senha')";
             if($connection->query($sql)){
                 echo "Usuário inserido com sucesso.";
             } else {
@@ -25,4 +25,26 @@
             }
         }
     }
+
+    try {
+    $conn = new PDO('mysql:host=localhost;dbname=si_pweb', $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+ 
+    $stmt = $conn->prepare('SELECT * FROM usuarios WHERE nome_usuario = :usuario');
+    $stmt->bindParam(':usuario', $usuario, PDO::PARAM_INT);
+    $stmt->execute();
+ 
+    $result = $stmt->fetchAll();
+ 
+    if ( count($result) ) {
+        foreach($result as $row) {
+          print_r($row);
+        }
+      } else {
+        echo "Nenhum resultado retornado.";
+      }
+    } catch(PDOException $e) {
+        echo 'ERROR: ' . $e->getMessage();
+    }
+
 ?>
