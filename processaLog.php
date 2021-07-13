7c62689ce07f251f545442c3873196534b11e45c
isabelvdsg <86683266+isabelvdsg@users.noreply.github.com>
2021-07-13 15:13:00 -0300

    Update processaLog.php

--- processaLog.php
+++ processaLog.php
@@ -1,1 +1,15 @@
 
+<? php
+    $user = $ _REQUEST [ "usuário" ];
+    $senha = $ _REQUEST [ "senha" ];
+    if ( $ user == "admin" && $ password == "123456" ) {
+        session_start ();
+        $ _SESSION [ "usuário" ] = $ usuário ;
+        $ _SESSION [ "senha" ] = $ senha ;
+        echo  "Usuário logado.";
+    <a  href = " exibir.php " > Exibir Usuário </ a> 
+    }else {
+        echo  "Acesso negado." ;
+    }
+    
+?>
