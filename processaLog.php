6fbef782b56c1346ecaade937bff22c6db9cf4ab
isabelvdsg <86683266+isabelvdsg@users.noreply.github.com>
2021-07-30 17:50:59 -0300

    Update processaLog.php

--- processaLog.php
+++ processaLog.php
@@ -1,5 +1,6 @@
 
 <? php
+    include 'processaCad.php';
     $user = $ _REQUEST [ "usuário" ];
     $senha = $ _REQUEST [ "senha" ];
     if ( $ user == "admin" && $ password == "123456" ) {
@@ -7,9 +8,19 @@
         $ _SESSION [ "usuário" ] = $ usuário ;
         $ _SESSION [ "senha" ] = $ senha ;
         echo  "Usuário logado.";
+
         $sql = "SELECT * FROM usuarios;";
+
     }else {
         echo  "Acesso negado." ;
+    }
+
+    $pesquisar = $_POST['pesquisar'];
+        $result_usuarios = "SELECT * FROM usuarios WHERE usuario LIKE '%$pesquisar%' LIMIT 5";
+        $resultado_usuarios = mysqli_query($conn, $result_usuarios);
+    
+     while($rows_usuarios = mysqli_fetch_array($resultado_usuarios)){
+        echo "Nome do usuário: ".$rows_usuarios['nome']."<br>";
     }
     
 ?>
